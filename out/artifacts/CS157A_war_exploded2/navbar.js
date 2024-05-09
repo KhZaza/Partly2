e5cd973be91babd05fb9a0e9c24f4ab586a25a9f
Khaled Zaza <95304594+KhZaza@users.noreply.github.com>
2024-05-09 14:09:11 -0700

    Delete out/artifacts/CS157A_war_exploded2 directory

--- out/artifacts/CS157A_war_exploded2/navbar.js
+++
@@ -1,7 +0,0 @@
-function hamburgerMenu(){
-    hamburger = document.querySelector(".hamburger")
-    hamburger.onclick = function() {
-        navBar = document.querySelector(".nav-bar")
-        navBar.classList.toggle("active");
-    }
-}
